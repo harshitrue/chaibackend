dfc400dc1ac42c4bad292d4c57738f6f4a91942d
harshitrue <harshitrue@gmail.com>
2025-06-08 22:52:12 +0530

    utilcodes

--- src/index.js
+++ src/index.js
@@ -9,15 +9,15 @@ dotenv.config({
      path: "./env"
      })
 connectDB()
-
-
-
-
-
-
-
-
-
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 8000}`);
+  });
+})
+.catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  });
 
 
 
@@ -42,8 +42,8 @@ const app = express();
       throw error;
     })
 
-    app.listen(process.env.PORT || 3000, () => {
-      console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
 
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,10 @@
+class ApiResponse {
+  constructor(statuscode, message="Success", data) {
+    this.statuscode = statuscode;
+    this.message = message;
+    this.data = data;
+    this.success = statuscode < 400; // Assuming status codes below 400 are successful
+  }
+
+
+}
