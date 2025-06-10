6f6292a5565497ca0e1dbb667859f8add1b53384
harshitrue <harshitrue@gmail.com>
2025-06-10 20:37:44 +0530

    logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,25 @@ import {uploadOnCloudinary} from '../utils/cloudinary.js';
 import {ApiResponse} from '../utils/ApiResponse.js';
 
 
+const generateAccessandRefreshTokens = async(userId) => {
+    try {
+        const user = await User.findById(userId);
+
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        // Save the refresh token in the user's document
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false });
+
+        return { accessToken, refreshToken };
+        
+    } catch (error) {
+            throw new ApiError(500, "Internal Server Error");
+            
+        }
+    };
+
 const registerUser = asyncHandler(async (req, res) => {
     //get user details from frontend
     //validate the user details
@@ -82,6 +101,81 @@ const registerUser = asyncHandler(async (req, res) => {
 })
 
 
+const loginUser = asyncHandler(async (req, res) => {
+    //req.body se data le aao
+    //username or email se user ko dhundo
+    //check if user exists
+    //check if password is correct
+    //access and refresh token generate karo
+    //send cookie
+    //return user details without password and refresh token
+    const {email, username, password} = req.body;
 
-export {registerUser}
-// export default registerUser
+    if(!email || !username) {
+        throw new ApiError(400, "Please provide email or username")
+    }
+
+    const user = await User.findOne({
+        $or: [{email}, {username}],
+    })
+
+    if(!user) {
+        throw new ApiError(404, "User not found")
+    }
+
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+    if(!isPasswordValid) {
+        throw new ApiError(401, "Invalid password")
+    }
+
+    const {accessToken,refreshToken} = await generateAccessandRefreshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id)
+    .select( "-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(new ApiResponse(200, "User logged in successfully", {
+        user :accessToken, loggedInUser, refreshToken
+    }
+)
+)
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+   await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true,
+            runValidators: true
+        }
+   )
+
+   const options = {
+    httpOnly: true,
+    secure: true
+   }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200,{}, "User logged out successfully"))
+})
+
+
+
+export {registerUser,loginUser,logoutUser}
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+import {asyncHandler} from '../utils/asyncHandler.js';
+import {ApiError} from '../utils/ApiError.js';
+import jwt from 'jsonwebtoken';
+import {User} from '../models/user.model.js';
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+try {
+        const token = req.cookies?.accessToken || req.header
+        ("Authorization")?.replace("Bearer", "")
+        //throw error if token is not present
+        if (!token) {
+            throw new ApiError("You are not authenticated");
+        }
+    
+        //verify the token
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    
+        const user = await User.findById(decodedToken?._id).select
+        ("-password =refreshToken");
+    
+        if (!user) {
+            throw new ApiError("User not found", 404);
+        }
+    
+        req.user = user;
+        next()
+} catch (error) {
+    throw new ApiError("You are not authenticated", 401);
+    
+}
+})
